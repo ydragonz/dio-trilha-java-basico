f0cd3809d5a652c44ffe2745e1be05eed254d6a9
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2024-05-18 13:03:24 -0300

    feat: add cadastro de nova conta

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numero;
+        String agencia, nome_cliente;
+        double saldo;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, insira os dados a seguir.\nNumero: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Agencia: ");
+        agencia = scanner.next();
+
+        System.out.println("Seu primeiro nome: ");
+        nome_cliente = scanner.next();
+
+        System.out.println("Saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("\n\nOla, " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agencia eh " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " ja esta disponivel para saque.");
     }
 }
